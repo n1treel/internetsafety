1e18283f54223c80b4bc8c7b91eac80e27a59435
Evgenii <74462629+n1treel@users.noreply.github.com>
2023-04-07 12:14:00 +0300

    Add files via upload

---
+++ generator.js
@@ -0,0 +1,11 @@
+function generatePassword() {
+    var length = 16;
+    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@$&";
+    var password = "";
+    for (var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    document.getElementById("password").value = password;
+  }
+  
